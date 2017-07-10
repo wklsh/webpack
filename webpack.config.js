4a1b23f0bc1563867ad350da13210cd6efd75752
shawn lim <shawn@codigo.sg>
2017-07-10 11:36:00 +0800

    webpack config overhual

--- webpack.config.js
+++ webpack.config.js
@@ -8,117 +8,31 @@ const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 const ResourceHintWebpackPlugin = require('resource-hints-webpack-plugin');
 const ImageminPlugin = require('imagemin-webpack-plugin').default;
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-
-/*----------------------------------------------------------------------------------*/ 
+/*-----------------------------------------------------------------------------*/ 
 // Webpack Bundle Analyzer *** UNCOMMENT THIS SECTION
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-/*----------------------------------------------------------------------------------*/ 
+/*------------------------------------------------------------------------------*/ 
 
 console.log("============================================================================");
-console.log("process.env.NODE_ENV: " + process.env.NODE_ENV);
+console.log("process.env.NODE_ENV: " + process.env.NODE_ENV.toUpperCase());
 console.log("============================================================================");
 
-const config = {
-	entry: {
-		index: './src/js/bundle-index.js',
-	},
 
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'js/bundle-[name].js',
-	},
 
-	devServer: {
-		contentBase: '.',
-		host: '0.0.0.0',
-		port: 3000,
-		disableHostCheck: true,
-	},
 
-	devtool: "#inline-source-map", // Default development sourcemap
-
-	module: {
-		rules: [
-			{
-                test: /\.scss$/,
-				use: ExtractTextPlugin.extract({
-					use: [
-						{
-							loader: 'css-loader',
-							options: {
-								modules: true,
-								importLoaders: 2,
-								localIdentName: '[local]',
-								sourceMap: true,
-								minimize: true,
-							}
-						},
-			            { loader: 'postcss-loader', options: { sourceMap: true, }},
-			            { loader: 'sass-loader', options: { sourceMap: true,}},
-					]
-				})
-			},
-			{
-				test: /\.html$/,
-				use: [
-					{
-						loader: 'html-loader',
-						options: {
-							minimize: true,
-							collapseWhitespace: false,
-							removeComments: false,
-							interpolate: true,
-						}
-					},
-				],
-			},
-			{
-				test: /\.js$/,
-				exclude: /node_module/,
-				use: [{
-					loader: 'babel-loader',
-					options: { presets: ['es2015'] },
-				}],
-			},
-			{
-				test: /\.(gif|png|jpe?g|svg)$/i,
-				use: [
-					{
-						loader: 'file-loader',
-						options: {
-							// name: '[name].[hash].[ext]',
-							name: '[name].[ext]',
-							outputPath: 'img/'
-						}
-					},
-				]
-			},
-		],
-	},
-
-	resolve: {
-	    alias: {
-	    	/*-------------------------------------------------------------------- 
-	    	 * // Instead of using relative paths when importing like so:
-			 *		import Utility from '../../utilities/utility';
-			 * // you can use the alias:
-			 *		import Utility from 'Vendor/utility';
-	    	-------------------------------------------------------------------- */
-	    	_components: path.resolve(__dirname, 'src/js/components/'),
-	    	_vendor: path.resolve(__dirname, 'src/js/vendor/')
-	    }
-	},
-
-	plugins: [
+/*------------------------------------------------------------------------------------------- 
+ * PLUGIN
+ *-----------------------------------------------------------------------------------------*/
+	const pluginsArray = [
 		//- Expose Dev/Prod Enviroment to JS
-		//----------------------------------------------------------------------
+		//-----------------------------------------------------------------------
 			new webpack.DefinePlugin({
 			    DEVELOPMENT: JSON.stringify(process.env.NODE_ENV === "development"),
 			    PRODUCTION: JSON.stringify(process.env.NODE_ENV === "production"),
 			}),
 
 		//- Split common chunks into a seperate JS vendor file
-		//----------------------------------------------------------------------
+		//-----------------------------------------------------------------------
 			new webpack.optimize.CommonsChunkPlugin({
 			  name: "vendor",
 			  // filename: "js/bundle-vendor.[hash].js",
@@ -127,15 +41,15 @@ const config = {
 			}),
 
 		//- Extract out CSS into a seperate file
-		//----------------------------------------------------------------------
+		//-----------------------------------------------------------------------
 			new ExtractTextPlugin({
 				// filename: 'main.[hash].css',
 				filename: 'bundle-scss.css',
 				allChunks: true,
 			}),
 
-		// Include JQuery
-		//----------------------------------------------------------------------
+		//- Include JQuery
+		//-----------------------------------------------------------------------
 			new webpack.ProvidePlugin({
 				$: 'jquery',
 				jQuery: 'jQuery',
@@ -143,19 +57,15 @@ const config = {
 		        'window.$': 'jquery',
 			}),
 
-		//- Remove Dist folder before compiling
-		//----------------------------------------------------------------------
-			new CleanWebpackPlugin(['dist']),
-
 		//- Prefetch JS files
-		//----------------------------------------------------------------------
+		//-----------------------------------------------------------------------
 			new HtmlWebpackPlugin({
 				prefetch: ['*.js', 'data.json'],
 				preload: '*.*'
 			}),
 
 		//- HTML Webpack individual files
-		//----------------------------------------------------------------------
+		//-----------------------------------------------------------------------
 			// HTML Files - Index
 				new HtmlWebpackPlugin({
 					chunks: ['vendor', 'index'],
@@ -163,16 +73,75 @@ const config = {
 					filename: 'index.html',
 					template: 'src/index.html'
 				}),
+			// HTML Files - functions
+				new HtmlWebpackPlugin({
+					chunks: ['vendor', 'functions'],
+					preload: false,
+					filename: 'functions.html',
+					template: 'src/functions.html'
+				}),
+			// HTML Files - pricing
+				new HtmlWebpackPlugin({
+					chunks: ['vendor', 'pricing'],
+					preload: false,
+					filename: 'pricing.html',
+					template: 'src/pricing.html'
+				}),
+			// HTML Files - about
+				new HtmlWebpackPlugin({
+					chunks: ['vendor', 'about'],
+					preload: false,
+					filename: 'about.html',
+					template: 'src/about.html'
+				}),
+			// HTML Files - download
+				new HtmlWebpackPlugin({
+					chunks: ['vendor', 'download'],
+					preload: false,
+					filename: 'download.html',
+					template: 'src/download.html'
+				}),
+			// HTML Files - contact
+				new HtmlWebpackPlugin({
+					chunks: ['vendor', 'contact'],
+					preload: false,
+					filename: 'contact.html',
+					template: 'src/contact.html'
+				}),
+			// HTML Files - partner
+				new HtmlWebpackPlugin({
+					chunks: ['vendor', 'partner'],
+					preload: false,
+					filename: 'partner.html',
+					template: 'src/partner.html'
+				}),
 
 		//- Favicon generation
-		//----------------------------------------------------------------------
+		//-----------------------------------------------------------------------
 			new FaviconsWebpackPlugin({
 				logo: './src/favicon.png',
 			    prefix: 'favico/',
 			}),
 
-		//- Image Optimisation- ensure plugin loads after any plugins that handles images 
-		//----------------------------------------------------------------------
+		//- UglifyJS
+		//-----------------------------------------------------------------------
+			new webpack.optimize.UglifyJsPlugin({ compress:{ warnings: false } }),
+
+		//- AggressiveMerging
+		//-----------------------------------------------------------------------
+			new webpack.optimize.AggressiveMergingPlugin(),
+
+		//- Copies individual files or entire directories to DIST without bundling
+		//-----------------------------------------------------------------------
+			new CopyWebpackPlugin([{ from: 'src/js/externals/*', to: 'js/externals/[name].[ext]' }]),
+
+		/*---------------------------------------------------------------------*/ 
+		// Webpack Bundle Analyzer *** UNCOMMENT THIS SECTION
+			// new BundleAnalyzerPlugin(),
+		/*---------------------------------------------------------------------*/ 
+
+		//- Image Optimisation- to be the last plugin to init 
+		//-----------------------------------------------------------------------
 			new ImageminPlugin({
 				disable: process.env.NODE_ENV == 'development',
 				test: 'img/***',
@@ -195,27 +164,126 @@ const config = {
 					},
 			}),
 
-		// UglifyJS
-		//----------------------------------------------------------------------
-			new webpack.optimize.UglifyJsPlugin({ compress:{ warnings: false } }),
-
-		// AggressiveMerging
-		//----------------------------------------------------------------------
-			new webpack.optimize.AggressiveMergingPlugin(),
-
-		//- Copies individual files or entire directories to DIST without bundling
-		//----------------------------------------------------------------------
-			new CopyWebpackPlugin([{ from: 'src/js/externals/*', to: 'js/externals/[name].[ext]' }]),
-
-		/*----------------------------------------------------------------------------------*/ 
-		// Webpack Bundle Analyzer *** UNCOMMENT THIS SECTION
-			// new BundleAnalyzerPlugin(),
-		/*----------------------------------------------------------------------------------*/ 
-
 		//- Init Prefetch JS
-		//----------------------------------------------------------------------
+		//-----------------------------------------------------------------------
 			new ResourceHintWebpackPlugin(),
 	]
-};
+
+	//- RUNS ONLY IN PRODUCTION MODE
+	if (process.env.NODE_ENV === "production") {
+	    pluginsArray.push(
+	    	//- Remove Dist folder before compiling
+			//-------------------------------------------------------------------
+			new CleanWebpackPlugin(['dist'])
+	    );
+	}
+
+
+
+
+
+
+/*------------------------------------------------------------------------------------------- 
+ * CONFIG
+ *-----------------------------------------------------------------------------------------*/
+	const config = {
+		entry: {
+			index: './src/js/bundle-index.js',
+			functions: './src/js/bundle-functions.js',
+			pricing: './src/js/bundle-pricing.js',
+			about: './src/js/bundle-about.js',
+			download: './src/js/bundle-download.js',
+			contact: './src/js/bundle-contact.js',
+			partner: './src/js/bundle-partner.js',
+		},
+
+		output: {
+			path: path.resolve(__dirname, 'dist'),
+			filename: 'js/bundle-[name].js',
+		},
+
+		devServer: {
+			contentBase: '.',
+			host: '0.0.0.0',
+			port: 3000,
+			disableHostCheck: true,
+		},
+
+		devtool: "#inline-source-map", // Default development sourcemap
+
+		module: {
+			rules: [
+				{
+	                test: /\.scss$/,
+					use: ExtractTextPlugin.extract({
+						use: [
+							{
+								loader: 'css-loader',
+								options: {
+									modules: true,
+									importLoaders: 2,
+									localIdentName: '[local]',
+									sourceMap: true,
+									minimize: true,
+								}
+							},
+				            { loader: 'postcss-loader', options: { sourceMap: true, }},
+				            { loader: 'sass-loader', options: { sourceMap: true,}},
+						]
+					})
+				},
+				{
+					test: /\.html$/,
+					use: [
+						{
+							loader: 'html-loader',
+							options: {
+								minimize: true,
+								collapseWhitespace: false,
+								removeComments: false,
+								interpolate: true,
+							}
+						},
+					],
+				},
+				{
+					test: /\.js$/,
+					exclude: /node_module/,
+					use: [{
+						loader: 'babel-loader',
+						options: { presets: ['es2015'] },
+					}],
+				},
+				{
+					test: /\.(gif|png|jpe?g|svg)$/i,
+					use: [
+						{
+							loader: 'file-loader',
+							options: {
+								// name: '[name].[hash].[ext]',
+								name: '[name].[ext]',
+								outputPath: 'img/'
+							}
+						},
+					]
+				},
+			],
+		},
+
+		plugins: pluginsArray,
+
+		resolve: {
+		    alias: {
+		    	/*--------------------------------------------------------------- 
+		    	 * // Instead of using relative paths when importing like so:
+				 *		import Utility from '../../utilities/utility';
+				 * // you can use the alias:
+				 *		import Utility from 'Vendor/utility';
+		    	---------------------------------------------------------------*/
+		    	_components: path.resolve(__dirname, 'src/js/components/'),
+		    	_vendor: path.resolve(__dirname, 'src/js/vendor/')
+		    }
+		},
+	};
 
 module.exports = config;
